803c3a0c20ecd4a39200e083bf66f07c56a7f22a
Olga Polishchuk <olgapolichuk@ukr.net>
2023-03-19 21:38:14 +0200

    add check exist name and notifications

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,28 +4,36 @@ import { Section } from './Section/Section';
 import { ContactsList } from './ContactsList/ContactsList';
 import { PhoneBoock } from './PhoneBoock/PhoneBoock';
 import { Filter } from './Filter/Filter';
+import 'react-notifications/lib/notifications.css';
+import {
+  NotificationContainer,
+  NotificationManager,
+} from 'react-notifications';
 
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
   addContact = ({ name, number }) => {
-  // addContact = ({ name }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
-
+    const checkName = this.state.contacts.find(
+      contact => contact.name === name
+    );
+    if (checkName) {
+      NotificationManager.warning(`${name} is already in contacts`)
+      return;
+    }
     this.setState(({ contacts }) => ({
       contacts: [contact, ...contacts],
     }));
@@ -35,6 +43,7 @@ class App extends Component {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
+    NotificationManager.error('One contact has been deleted');
   };
 
   changeFilter = e => {
@@ -44,7 +53,6 @@ class App extends Component {
   getVisibleContacts = () => {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
-    // console.log(contacts);
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
@@ -57,18 +65,22 @@ class App extends Component {
 
     return (
       <>
-      <Section title="Phonebook">
-        <PhoneBoock onSubmit={this.addContact} />
+        <Section title="Phonebook">
+          <PhoneBoock onSubmit={this.addContact} />
         </Section>
         <Section title="Contacts">
+          <Filter
+            value={filter}
+            onChange={this.changeFilter}
+            totalcontactsCount={totalcontactsCount}
+          />
 
-        <Filter value={filter} onChange={this.changeFilter} totalcontactsCount={totalcontactsCount}/>
-
-        <ContactsList
-          contacts={visibleContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </Section>
+          <ContactsList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+        <NotificationContainer />
       </>
     );
   }
